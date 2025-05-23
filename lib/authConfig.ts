a8a5343a92d334939de8f39c9e88bd794f428ac6
Fujiwara0805 <tiki4091@gmail.com>
2025-05-23 12:02:10 +0900

    ✨ 名刺カードのUIを改善し、アニメーションを適用しました。また、ファイルの更新日時表示を日本語に対応させ、全体的にシンプルでクールなデザインを維持しました。認証設定のスコープを更新し、Google Driveのファイルアクセスを最適化しました。

--- lib/authConfig.ts
+++ lib/authConfig.ts
@@ -15,8 +15,7 @@ export const nextAuthConfiguration: NextAuthOptions = {
             "openid",
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/drive.file",
-            "https://www.googleapis.com/auth/spreadsheets"
+            "https://www.googleapis.com/auth/drive.file"
           ].join(" "),
         },
       },
